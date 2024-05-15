014791aff329fa316cb6c643490150f3c0e9d129
madjick1 <jessenguyen1112@gmail.com>
2024-05-14 17:26:46 -0700

    Modified Player to have Item pointers instead of object

--- header/Item.h
+++ header/Item.h
@@ -1,5 +1,52 @@
+#include <string>
+#include "../mythikin.h"
+
 #pragma once
 
-class Item{
-  //Another stub
+using namespace std;
+
+class Item
+{
+protected:
+    string name;
+    unsigned quantity;
+    unsigned capacity;
+
+public:
+    Item(string name, unsigned quant, unsigned cap) : name(name), quantity(quant), capacity(cap)
+    {
+        // capacity can not be less that quanitity
+        if (capacity < quantity)
+        {
+            capacity = quantity;
+        }
+    };
+
+    // Accessor methods
+    string getName() const { return name; }
+
+    unsigned getQuantity() const { return quantity; }
+
+    unsigned getCapacity() const { return capacity; }
+
+    // Mutator methods
+    void nameItem(string newName) { name = newName; }
+
+    void changeQuantity(unsigned newQuant)
+    {
+        if (newQuant >= 0)
+        {
+            quantity = newQuant;
+        }
+    }
+
+    void changeCapacity(unsigned newCap)
+    {
+        if (newCap >= quantity && newCap >= 0)
+        {
+            capacity = newCap;
+        }
+    }
+
+    virtual void use(Mythikin&) = 0;
 };
--- header/Player.h
+++ header/Player.h
@@ -7,13 +7,40 @@
 #include "./Item.h"
 using std::string;
 
-class Player {
-  private:
-    string name;
-    Location currentLocation;
-    vector<Item> items;
-    int mythicoins;
-  public:
-    const string& getName() {return name;}
-    const Location& getLocation() {return currentLocation;}
+class Player
+{
+private:
+  string name;
+  Location currentLocation;
+  vector<Mythikin> team;
+  vector<Item*> items;
+  int mythicoins;
+
+public:
+  Player(const string &name = "No Name", const Location &currentLocation = Location(), const vector<Mythikin> &team = vector<Mythikin>(), const vector<Item*> &items = vector<Item*>(), int mythicoins = 0) 
+   : name(name), currentLocation(currentLocation), team(team), items(items), mythicoins(mythicoins) // Parameterized with some default values
+  {
+  }
+
+  
+
+  const string &getName() const { return name; }
+  const Location &getLocation() const { return currentLocation; }
+  const vector<Mythikin> &getTeam() const { return team; }
+  const vector<Item*> &getInventory() const { return items; }
+  int getCoins() const { return mythicoins; }
+
+  void setName(const string &newName) { name = newName; }
+  void setLocation(const Location &newLocation) { currentLocation = newLocation; }
+  void setTeam(vector<Mythikin> newTeam) { team = newTeam; }
+  void setItems(vector<Item*> newInventory) { items = newInventory; }
+
+  void addMythikin(Mythikin &newKin) { team.push_back(newKin); } // Adds the parameter Mythikin to the team
+  void removeMythikin(const string &mythiName);                  // Uses the name of the mythikin to remove it from team
+
+  void addItem(Item* newItem) { items.push_back(newItem); } // Adds the parameter item to items vector
+  void removeItem(const string &itemName);                  // Uses the name of the item to remove it from the items vector
+
+  void addCoins(int amnt) { mythicoins += amnt; }
+  void removeCoins(int amnt) { mythicoins -= amnt; }
 };
