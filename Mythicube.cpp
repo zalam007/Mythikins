cb17d1a07638db88663b44d5350499d1d68c9439
AxelPere <paxel1001@gmail.com>
2024-05-19 16:16:43 -0700

    Created mythicube

--- Mythicube.cpp
+++ Mythicube.cpp
@@ -1,17 +1,21 @@
 #include "Mythicube.h"
 
 Mythicube::Mythicube(string name, unsigned quant, unsigned cap, unsigned rate)
-    : Item(name, quant, cap), catchRate(rate){}
+                    : Item(name, quant, cap), catchRate(rate){}
 
 bool Mythicube::caught(Mythikin &victim) const
 {
     // Calculate percentage of health the victim has
-    double hpPercent = ((victim.getHP()) / static_cast<double>(victim.getMaxHP())) * catchRate;
+    double hpPercent = ((victim.getHP()) / static_cast<double>(victim.getMaxHP())) * 100;
 
-    double chance = 0.25 + static_cast<double>(rand() / 5) / (RAND_MAX); // 0.25 to 0.5
+    double chance = 11 + static_cast<double>(rand() % 41); // 0.25 to 0.5
 
-    if (hpPercent <= chance)
-    { // if HP percent is atleast 50% or lower, the victim is catchable
+cout << "HP: " << hpPercent << endl;
+cout << "chance: " << chance << endl;
+cout << "Success?: " << hpPercent / catchRate << "<=" << chance - LV/10 << endl;
+
+    if (hpPercent / catchRate <= chance - LV/10)
+    {
         return true;
     }
     return false;
@@ -23,5 +27,5 @@ void Mythicube::use(Mythikin &victim)
     {
         //Incomplete
     }
-    changeQuantity(getQuantity() - 1);
+    //changeQuantity(getQuantity() - 1);
 }
