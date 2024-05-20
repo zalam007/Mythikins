937951305fa688347951be14165c31292f4682c4
Dylan Wong <dylanwong6605@gmail.com>
2024-05-19 21:27:10 -0700

    added location class

--- location.h
+++ location.h
@@ -1,21 +1,72 @@
 #pragma once
 
 #include <vector>
-#include "mythikin.h"
+#include <string>
+#include "NPC.h"
 using namespace std;
 
-class Location {
+class Location
+{
 private:
     string name;
-    vector<Mythikin> MythikinSpawn;
-public:
-    Location(const string& name, const vector <Mythikin>& Mythikins) : name(name), MythikinSpawn(Mythikins) {}
+    vector<Location *> neighborLocations;
+    vector<NPC> NPCSpawn;
 
-    string getName() {
+public:
+    Location(const string &name) : name(name) {}
+
+    Location(const string &name, const vector<Location *> &neighbors, const vector<NPC> &npcs) : name(name), neighborLocations(neighbors), NPCSpawn(npcs) {}
+
+    string getName()
+    {
         return name;
     }
 
-    void setMythikins(const vector <Mythikin>& Mythikins) {
-        MythikinSpawn = Mythikins;
+    vector<Location *> getNeighbors()
+    {
+        return neighborLocations;
+    }
+
+    vector<NPC> getNPCList()
+    {
+        return NPCSpawn;
+    }
+
+    void setName(const string &name)
+    {
+        this->name = name;
+    }
+
+    void addNPC(const NPC &npc)
+    {
+        NPCSpawn.push_back(npc);
+    }
+
+    void removeNPC(const string &name)
+    {
+        for (int i = 0; i < NPCSpawn.size(); ++i)
+        {
+            if (NPCSpawn[i].getName() == name)
+            {
+                NPCSpawn.erase(NPCSpawn.begin() + i);
+            }
+        }
+    }
+
+    void addLocation(const Location *&location)
+    {
+        neighborLocations.push_back(location);
+    }
+
+    void removeLocation(const string &name)
+    {
+        for (int i = 0; i < neighborLocations.size(); ++i)
+        {
+            if (neighborLocations[i]->getName() == name)
+            {
+                delete neighborLocations[i];
+                neighborLocations.erase(neighborLocations.begin() + i);
+            }
+        }
     }
 };
