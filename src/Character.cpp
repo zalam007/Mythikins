bcb933012ae91b474dc2f0f03f48e7db5b552f95
madjick1 <jessenguyen1112@gmail.com>
2024-05-22 20:56:56 -0700

    Added Team and updated Character, NPC, and Player class to accept Team

--- src/Character.cpp
+++ src/Character.cpp
@@ -3,26 +3,11 @@
 
 using std::string;
 
-void Character::addMythikin(const Mythikin& newMythikin) {
-  team.push_back(newMythikin);
-}
-
-void Character::removeMythikin(const string& name) {
-  for (unsigned i = 0; i < team.size(); i++) {
-    if(team.at(i).getName() == name) {
-      team.erase(team.begin() + i);
-    }
-  }
-  //Does nothing if name not found
-}
-
-void Character::useItem(Item* itemBeingUsed, const string& mythikinName) { //Feel like it'd be better to use the name of the item here
+void Character::useItem(const string& itemBeingUsed, const string& mythikinName) { //Feel like it'd be better to use the name of the item here
   for (unsigned i = 0; i < items.size(); i++) {
-    if (items.at(i)->getName() == itemBeingUsed->getName()) { //Check if name of param matches name in inventory
-      for (unsigned i = 0; i < team.size(); i++) { //Searches team for the right mythikin the item will be used on
-        if (team.at(i).getName() == mythikinName) {
-          items.at(i)->use(team.at(i));
-        }
+    if (items.at(i)->getName() == itemBeingUsed) { //Check if name of param matches name in inventory
+      for (unsigned j = 0; j < team.getSize(); j++) { //Searches team for the right mythikin the item will be used on
+        items.at(i)->use(team.getSlot(j));
       }
       if (items.at(i)->getQuantity() == 0) { //If quantity is zero, delete it from inventory
         items.erase(items.begin() + i);
---
+++ src/Team.cpp
@@ -0,0 +1,39 @@
+#include "../header/Team.h"
+#include <stdexcept>
+
+Mythikin& Team::getSlot(int index) {
+  if (index < 0 || index > teamSize) //Bound checking, but hopefully not needed
+  {
+    throw runtime_error("Index out of bounds"); 
+  }
+  return team[index];
+}
+
+void Team::swapSlots(int slot1, int slot2) {
+  if (slot1 < 0 || slot1 > teamSize || slot2 < 0 || slot2 > teamSize)
+  {
+    throw runtime_error("Index out of bounds");
+  }
+  Mythikin swapped = team[slot1];
+  team[slot1] = team[slot2];
+  team[slot2] = swapped;
+}
+
+void Team::addMythikin(Mythikin newMember) {
+  if (teamSize == MAX_SIZE) throw runtime_error("Team Full"); //Check if full
+  team[teamSize] = newMember;
+  teamSize++;
+}
+
+void Team::removeMythikin(const string& name) {
+  for (unsigned i = 0; i < teamSize; i++) {
+    if(team[i].getName() == name)
+    {
+      for (int j = i; j < teamSize - 1; j++) { // Deletes the element at i
+        team[j] = team[j+1]; 
+      }
+      teamSize--;
+    }
+  }
+  //Does nothing if name not found
+}
