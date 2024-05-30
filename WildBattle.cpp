4178dfbf1227c1ae9f5977aa3fed882396a4e3c7
Zami Alam <zalam007@ucr.edu>
2024-05-29 22:06:17 -0700

    updated wild battle isover iswon, wildBattle should be done (not fully tested)

--- WildBattle.cpp
+++ WildBattle.cpp
@@ -1,37 +1,51 @@
 #include "WildBattle.h"
 
 bool WildBattle::isOver() {
-    // Call the base class isOver() to check if player's team is knocked out
-    if (Battle::isOver())
-        return true;
-
-    // Check if the wild Mythikin was successfully caught
+    // Return true if the wild Mythikin was successfully caught
     if (myMythicube.getCapturedMythikin() != nullptr)
         return true;
+    
+    // Check if all player's Mythikins are knocked out
+    bool playerAllKnockedOut = true;
+    for (int i = 0; i < Battle::playerTeam.getSize(); ++i) {
+        if (Battle::playerTeam.getSlot(i).getHP() > 0) {
+            playerAllKnockedOut = false;
+            break;
+        }
+    }
 
-    return false;
+    // Check if the wild Mythikin is knocked out
+    bool npcKnockedOut = true;
+    if (wildMythikin.getHP() <= 0)
+        npcKnockedOut = false;
+
+    return playerAllKnockedOut || npcKnockedOut; // Return true if either party is all knocked
 }
 
 bool WildBattle::ifWon() {
-    // Call the base class ifWon() to check if the player won the battle normally
-    if (Battle::ifWon())
-        return true;
-
-    // Check if the wild Mythikin was successfully caught
+    // Return true if the wild Mythikin was successfully caught
     if (myMythicube.getCapturedMythikin() != nullptr)
         return true;
 
-    return false;
+    // Check if any player's Mythikins are still alive
+    bool playerHasAlive = false;
+    for (int n = 0; n < playerTeam.getSize(); n++) {
+        if (playerTeam.getSlot(n).getHP() > 0) {
+            playerHasAlive = true;
+            break;
+        }
+    }
+
+    return isOver() && playerHasAlive;
 }
 
-void WildBattle::tryToCatch() {
+
+void WildBattle::catchMythikin() {
     myMythicube.use(wildMythikin); // Call the use method of Mythicube to attempt catching the wild Mythikin
 }
 
+// 25% chance of fleeing
 bool WildBattle::Flee() {
-    // Generate a random number between 0 and 3
-    int randomNum = rand() % 4;
-
-    // If the random number is 0, return true (25% chance of fleeing)
-    return randomNum == 0;
+    int randomNum = rand() % 4; // Generate a random number between 0 and 3
+    return randomNum == 0; // If the random number is 0, return true (25% chance of fleeing)
 }
--- WildBattle.h
+++ WildBattle.h
@@ -5,18 +5,16 @@
 
 class WildBattle : public Battle {
 public:
-    WildBattle(Team& playerTeam, Team& npcTeam, Mythikin& wildMythikin, Mythicube& myMythicube) 
-        : Battle(playerTeam, npcTeam), wildMythikin(wildMythikin), myMythicube(myMythicube) { srand(time(0)); }
+    //Constructor to set team & wild Mythikin & seed
+    WildBattle(Team& playerTeam, Mythikin& wildMythikin, Mythicube& myMythicube) 
+        : Battle(playerTeam), wildMythikin(wildMythikin), myMythicube(myMythicube) { srand(time(0)); }
 
-    // Override isOver to consider battle end if player team is knocked out
-    bool isOver() override;
+    bool isOver() override; // Override isOver to consider battle end if player team is knocked out
+    bool ifWon() override;  // Override ifWon to always return false in WildBattle
 
-    // Override ifWon to always return false in WildBattle
-    bool ifWon() override;
-
-    void tryToCatch();
-    bool Flee();
-    private:
+    void catchMythikin();   // catch the wild Mythikin with the Mythicube
+    bool Flee();    // 25% chance of fleeing
+private:
     Mythikin& wildMythikin; // Wild Mythikin to battle
     Mythicube& myMythicube; // Reference to the player's Mythicube
 };
