7e8d518bfb0d9ab3c2705597763f9ff22c6a1755
AxelPere <paxel1001@gmail.com>
2024-06-02 15:16:46 -0700

    Small changes, added cout and removed player pointer

--- src/PC.cpp
+++ src/PC.cpp
@@ -1,28 +1,42 @@
 #include "../header/PC.h"
 
-void PC::depositMythikin(Mythikin &mover)
+~PC() // Deletes entire PC storage.
 {
-    if (user->getTeam().getSize() > 1)
+    for (int i = 0; i < mythikinPCStorage.size(); ++i)
+    {
+        delete mythikinPCStorage[i]; // Each deposited mythikin in the PC is deleted.
+    }
+}
+
+void PC::depositMythikin(Mythikin &mover, Team &userTeam)
+{
+    if (userTeam.getSize() > 1)
     {                                                     // Only able to deposit as long as there is more than one Mythikin in the team.
         mythikinPCStorage.push_back(new Mythikin(mover)); // Adds a new Mythikin to the PC.
-        user->getTeam().removeMythikin(mover.getName());  // Removes the Mythikin from the team.
+        userTeam.removeMythikin(mover.getName());         // Removes the Mythikin from the team.
+        return;
     }
     // Nothing if the team is empty or only has one Mythikin.
+    cout << "You can not deposit your one Mythikin in your team!" << endl;
 }
 
 void PC::withdrawMythikin(const string name)
 {
-    if (user->getTeam().getSize() <= user->getTeam().getMaxSize())
+    if (userTeam.getSize() <= user->getTeam().getMaxSize())
     {                                                      // Only able to withdraw as long as the team is not full.
         for (int i = 0; i < mythikinPCStorage.size(); ++i) // Iterates through the PC storage.
         {
             if (mythikinPCStorage[i]->getName() == name) // If found the Mythikin by its name
             {
-                user->getTeam().addMythikin(*mythikinPCStorage[i]);     // Adds the Mythikin to the team.
+                userTeam.addMythikin(*mythikinPCStorage[i]);            // Adds the Mythikin to the team.
                 mythikinPCStorage.erase(mythikinPCStorage.begin() + i); // Removes the Mythikin from the PC.
+                return;
             }
         }
         // Nothing if the Mythikin is not found.
+        cout << "Mythikin not found!" << endl;
+        return;
     }
     // Nothing if the team is full.
+    cout << "Team is full. No space to withdraw from PC into your team!" << endl;
 }
