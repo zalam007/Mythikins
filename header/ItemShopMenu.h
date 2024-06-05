fd91ebae66f8e4ee6abca28f984087855f4a5753
AxelPere <paxel1001@gmail.com>
2024-06-05 14:17:04 -0700

    added comments in ItemShopMenu.h

--- header/ItemShopMenu.h
+++ header/ItemShopMenu.h
@@ -8,9 +8,11 @@ using namespace std;
 class ItemShopMenu : protected Menu
 {
 public:
-    void printMenu(Mythishop &shop, Player &shopper);
+    void printMenu(Mythishop &shop, Player &shopper); // Main menu for item shop. User can sell or buy items.
 
 private:
+    // Helper functions. Sub-menus of printMenu.
+
     void buyMenu(Mythishop &shop, Player &shopper);
 
     void sellMenu(Player &shopper);
