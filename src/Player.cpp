7d6a6bd2cfce960dcdf53de9ab0da438a0ccba6f
madjick1 <jessenguyen1112@gmail.com>
2024-06-07 10:06:18 -0700

    Fixed a memory leak regarding player inventory

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,7 +4,9 @@
 void Player::removeItem(const string& itemName) {
   for (unsigned i = 0; i < items.size(); i++) {
     if(items.at(i)->getName() == itemName) {
+      Item* victim = items.at(i);
       items.erase(items.begin() + i);
+      delete victim;
     }
   }
   //Does nothing if name not found
